97fdce3f5942fdd39342cb2601cc01c10fd5e40c
Marek Dovjak <mdovjak@sygic.com>
2018-11-16 09:19:23 +0100

    Add temporary sdk initialization until browse map module is ready

--- Example/SygicMapsKit/SygicMapsAPIConfig.swift
+++ Example/SygicMapsKit/SygicMapsAPIConfig.swift
@@ -1,6 +1,6 @@
 struct SygicMapsAPI {
     
-    static let appKey = ""
-    static let appSecret = ""
+    static let appKey = "sdk-test"
+    static let appSecret = "rPMNrPw6PKiZiWmc17gHu7IVzOnjdAkqGUwqXPr+IG69ExNDMJZ/M7NfSg7itsuvRbcJlcTp6vX6R/bvF4MEWw=="
     
 }
--- Example/SygicMapsKit/ViewController.swift
+++ Example/SygicMapsKit/ViewController.swift
@@ -1,16 +1,35 @@
 import UIKit
+import SygicMaps
+import SygicUIKit
 
 class ViewController: UIViewController {
-
+    var mapView: SYMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if SYContext.isInitialized() {
+            addMapView()
+        } else {
+            NotificationCenter.default.addObserver(self, selector: #selector(self.addMapView), name: Notification.Name("SDKdone"), object: nil)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc private func addMapView() {
+        mapView = SYMapView(frame: view.frame, geoCenter: SYGeoCoordinate(latitude: 48.14816, longitude: 17.10674)!, rotation: 0, zoom: 15, tilt: 0)
+        guard let symapView = mapView  else {
+            return
+        }
+        
+        view.addSubview(symapView)
+        symapView.translatesAutoresizingMaskIntoConstraints = false;
+        symapView.coverWholeSuperview()
+    }
 }
 
