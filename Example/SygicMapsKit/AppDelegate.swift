4db92090ae0a4e8ed14aaa4fa8569472c67df0d2
Marek Dovjak <mdovjak@sygic.com>
2018-11-19 14:09:08 +0100

    remove key/secret

--- Example/SygicMapsKit/AppDelegate.swift
+++ Example/SygicMapsKit/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        SYMKApiKeys.set(appKey: "sdk-test", appSecret: "rPMNrPw6PKiZiWmc17gHu7IVzOnjdAkqGUwqXPr+IG69ExNDMJZ/M7NfSg7itsuvRbcJlcTp6vX6R/bvF4MEWw==")
+        SYMKApiKeys.set(appKey: "", appSecret: "")
         return true
     }
 
