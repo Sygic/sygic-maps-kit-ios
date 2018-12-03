4ca680917ddedf856daf5334de9602f5911f8db6
Jakub Kracina <jkracina@sygic.com>
2018-12-03 10:55:19 +0100

    Zoom control added to Browse Map module

--- SygicMapsKit/Classes/BrowseMap/SYMKBrowseMapView.swift
+++ SygicMapsKit/Classes/BrowseMap/SYMKBrowseMapView.swift
@@ -12,6 +12,10 @@ class SYMKBrowseMapView: UIView {
 
     private let sideMargin: CGFloat = 16
     
+    private weak var compass: UIView?
+    private weak var recenterButton: UIView?
+    private weak var zoomControl: UIView?
+    
     // MARK: - Public Methods
     
     public override init(frame: CGRect) {
@@ -36,14 +40,27 @@ class SYMKBrowseMapView: UIView {
         mapView.coverWholeSuperview()
     }
     
-    public func setupCompass(_ compass: SYUICompass) {
+    public func setupCompass(_ compass: UIView) {
+        self.compass = compass
         addSubview(compass)
         bringSubview(toFront: compass)
         compass.trailingAnchor.constraint(equalTo: safeTrailingAnchor, constant: -sideMargin).isActive = true
         compass.topAnchor.constraint(equalTo: safeTopAnchor, constant: sideMargin).isActive = true
     }
     
-    public func setupRecenter(_ recenter: SYUIActionButton) {
+    public func setupZoomControl(_ zoomControl: UIView) {
+        self.zoomControl = zoomControl
+        addSubview(zoomControl)
+        bringSubview(toFront: zoomControl)
+        zoomControl.translatesAutoresizingMaskIntoConstraints = false
+        zoomControl.leadingAnchor.constraint(equalTo: safeLeadingAnchor, constant: sideMargin).isActive = true
+        if let recenterButton = recenterButton {
+            zoomControl.bottomAnchor.constraint(equalTo: recenterButton.topAnchor, constant: -16).isActive = true
+        }
+    }
+    
+    public func setupRecenter(_ recenter: UIView) {
+        recenterButton = recenter
         addSubview(recenter)
         bringSubview(toFront: recenter)
         recenter.translatesAutoresizingMaskIntoConstraints = false
