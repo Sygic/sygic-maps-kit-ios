670ae54f31da917cb472e3450f3a05ed9cac1e68
Jakub Kracina <jkracina@sygic.com>
2018-12-12 14:18:15 +0100

    Recenter State computed preporties moved to RecenterController class as MapState extension

--- SygicMapsKit/Classes/Map/SYMKMapState.swift
+++ SygicMapsKit/Classes/Map/SYMKMapState.swift
@@ -7,6 +7,7 @@ internal protocol MapControl {
 }
 
 public class SYMKMapState {
+    
     var geoCenter: SYGeoCoordinate = SYGeoCoordinate(latitude: 48.147, longitude: 17.101878)!
     var zoom: CGFloat = 16
     var rotation: CGFloat = 0
@@ -18,26 +19,5 @@ public class SYMKMapState {
     var isTilt3D: Bool {
         return tilt >= 0.01
     }
-    
-    var recenterCurrentState: SYMKMapRecenterController.state {
-        if cameraMovementMode == .free {
-            return .free
-        } else {
-            if cameraRotationMode == .attitude {
-                return .lockedCompass
-            } else {
-                return .locked
-            }
-        }
-    }
-    
-    var recenterStates: [SYMKMapRecenterController.state] {
-        switch recenterCurrentState {
-        case .free:
-            return [.free, .locked]
-        case .locked,
-             .lockedCompass:
-            return [.locked, .lockedCompass]
-        }
-    }
+
 }
--- SygicMapsKit/Classes/RecenterControl/SYMKMapRecenterController.swift
+++ SygicMapsKit/Classes/RecenterControl/SYMKMapRecenterController.swift
@@ -9,3 +9,29 @@ public class SYMKMapRecenterController: SYUIMapRecenterController, MapControl {
     }
     
 }
+
+extension SYMKMapState {
+    
+    var recenterCurrentState: SYMKMapRecenterController.state {
+        if cameraMovementMode == .free {
+            return .free
+        } else {
+            if cameraRotationMode == .attitude {
+                return .lockedCompass
+            } else {
+                return .locked
+            }
+        }
+    }
+    
+    var recenterStates: [SYMKMapRecenterController.state] {
+        switch recenterCurrentState {
+        case .free:
+            return [.free, .locked]
+        case .locked,
+             .lockedCompass:
+            return [.locked, .lockedCompass]
+        }
+    }
+    
+}
