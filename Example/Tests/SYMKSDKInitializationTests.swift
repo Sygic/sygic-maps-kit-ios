0e46369740bce185176013cfe3b1235ad0826e88
Jakub Cali­k <jcalik@sygic.com>
2019-01-22 10:53:47 +0100

    no keys public

--- Example/Tests/SYMKSDKInitializationTests.swift
+++ Example/Tests/SYMKSDKInitializationTests.swift
@@ -19,25 +19,25 @@ class SDKInitializationTest: QuickSpec {
                 expect(initializationResult).toEventually(equal(false), timeout: 5)
             }
             
-            it("should not finish with incorrect key") {
-                var initializationResult: Bool?
-                // incorrect key
-                SYMKApiKeys.set(appKey: "sygic.ios.maps.sampleees", appSecret: "/nisSdnGUzpZ7XI+h7+lna3IslJfNTHIxyiZlpjgR7aX1Nz12345Z4rVJtLZkOxyh0AL1q+k0qhHshga12d/jg==")
-                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
-                    initializationResult = result
-                })
-                expect(initializationResult).toEventually(equal(false), timeout: 10)
-            }
-            
-            it("should initialize succesfully") {
-                var initializationResult: Bool?
-                // correct test key
-                SYMKApiKeys.set(appKey: "sygic.ios.maps.sample", appSecret: "/nisSdnGUzpZ7XI+h7+lna3IslJfNTHIxyiZlpjgR7aX1Nz9jaw4Z4rVJtLZkOxyh0AL1q+k0qhHshga12d/jg==")
-                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
-                    initializationResult = result
-                })
-                expect(initializationResult).toEventually(equal(true), timeout: 10)
-            }
+//            it("should not finish with incorrect key") {
+//                var initializationResult: Bool?
+//                // incorrect key
+//                SYMKApiKeys.set(appKey: "", appSecret: "")
+//                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
+//                    initializationResult = result
+//                })
+//                expect(initializationResult).toEventually(equal(false), timeout: 10)
+//            }
+//            
+//            it("should initialize succesfully") {
+//                var initializationResult: Bool?
+//                // correct test key
+//                SYMKApiKeys.set(appKey: "", appSecret: "")
+//                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
+//                    initializationResult = result
+//                })
+//                expect(initializationResult).toEventually(equal(true), timeout: 10)
+//            }
             
             afterEach {
                 SYMKSdkManager.shared.terminate()
