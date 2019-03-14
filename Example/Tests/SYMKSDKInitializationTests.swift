457fcd80e8db9cccfd5e9a5b93fa9f3d516168d0
Jakub Cali­k <jcalik@sygic.com>
2019-03-14 14:49:32 +0100

    SDK init tests

--- Example/Tests/SYMKSDKInitializationTests.swift
+++ Example/Tests/SYMKSDKInitializationTests.swift
@@ -19,25 +19,28 @@ class SDKInitializationTest: QuickSpec {
                 expect(initializationResult).toEventually(equal(false), timeout: 5)
             }
             
-//            it("should not finish with incorrect key") {
-//                var initializationResult: Bool?
-//                // incorrect key
-//                SYMKApiKeys.set(appKey: "", appSecret: "")
-//                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
-//                    initializationResult = result
-//                })
-//                expect(initializationResult).toEventually(equal(false), timeout: 10)
-//            }
-//            
-//            it("should initialize succesfully") {
-//                var initializationResult: Bool?
-//                // correct test key
-//                SYMKApiKeys.set(appKey: "", appSecret: "")
-//                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
-//                    initializationResult = result
-//                })
-//                expect(initializationResult).toEventually(equal(true), timeout: 10)
-//            }
+            it("should not finish with incorrect key") {
+                var initializationResult: Bool?
+                // incorrect key
+                let appKey = ProcessInfo.processInfo.environment["SDK_APP_KEY"] ?? ""
+                SYMKApiKeys.set(appKey: appKey, appSecret: "fweferfSdnGUzpZ7XI+h7+lna3IslJrewfNTHIxyiZlpjgR7aX1Nz9jaw4Z4rVJtLZkOxyh0AL1q+dwefwfwf")
+                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
+                    initializationResult = result
+                })
+                expect(initializationResult).toEventually(equal(false), timeout: 10)
+            }
+            
+            it("should initialize succesfully") {
+                var initializationResult: Bool?
+                // correct test key
+                let appKey = ProcessInfo.processInfo.environment["SDK_APP_KEY"] ?? ""
+                let appSecret = ProcessInfo.processInfo.environment["SDK_APP_SECRET"] ?? ""
+                SYMKApiKeys.set(appKey: appKey, appSecret: appSecret)
+                SYMKSdkManager.shared.initializeIfNeeded({ (result) in
+                    initializationResult = result
+                })
+                expect(initializationResult).toEventually(equal(true), timeout: 10)
+            }
             
             afterEach {
                 SYMKSdkManager.shared.terminate()
