8b913cde21f633de0b9928414eb1ce63bbfe6c75
Jakub Kracina <jkracina@sygic.com>
2019-03-15 19:22:36 +0100

    Search Module
    - search view controller (search module)
    - search view for search module (search bar & results table view)
    - example browse map with search button
    - dummy class for search model

--- SygicMapsKit/Classes/SearchModule/SYMKSearchView.swift
+++ SygicMapsKit/Classes/SearchModule/SYMKSearchView.swift
@@ -1,3 +1,24 @@
+//// SYMKSearchView.swift
+//
+// Copyright (c) 2019 - Sygic a.s.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the &quot;Software&quot;), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 
 class SYMKSearchView: UIView {
@@ -20,6 +41,15 @@ class SYMKSearchView: UIView {
         self.init(frame: .zero)
     }
     
+    public func setupSearchBarView(_ searchView: UIView) {
+        searchBarView = searchView
+        searchView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(searchView)
+        searchView.topAnchor.constraint(equalTo: safeTopAnchor).isActive = true
+        searchView.leadingAnchor.constraint(equalTo: safeLeadingAnchor).isActive = true
+        searchView.trailingAnchor.constraint(equalTo: safeTrailingAnchor).isActive = true
+    }
+    
     public func setupResultsView(_ resultsView: UIView) {
         searchResultsView = resultsView
         resultsView.translatesAutoresizingMaskIntoConstraints = false
@@ -38,14 +68,6 @@ class SYMKSearchView: UIView {
     
     private func setupUI() {
         backgroundColor = .white
-        
-        let searchBar = UISearchBar(frame: .zero)
-        searchBarView = searchBar
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(searchBar)
-        searchBar.topAnchor.constraint(equalTo: safeTopAnchor).isActive = true
-        searchBar.leadingAnchor.constraint(equalTo: safeLeadingAnchor).isActive = true
-        searchBar.trailingAnchor.constraint(equalTo: safeTrailingAnchor).isActive = true
     }
     
 }
