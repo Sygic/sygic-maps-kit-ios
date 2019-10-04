87e12c28d0fff4fc189856f94718f03bc25f9f40
jcalik <jcalik@sygic.com>
2019-10-04 15:12:42 +0200

    Route options table

--- SygicMapsKit/Classes/RoutePlannerModule/Options/SYMKRouteOptionsViewController.swift
+++ SygicMapsKit/Classes/RoutePlannerModule/Options/SYMKRouteOptionsViewController.swift
@@ -20,14 +20,16 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Foundation
+import UIKit
 import SygicMaps
 import SygicUIKit
 
 
-public class SYMKRouteOptionsViewController: UIViewController {
+public class SYMKRouteOptionsViewController: UITableViewController {
     
     private var options = SYRoutingOptions()
+    
+    private var allAvodsList: [SYAvoidType] = [.toll, .ferries, .motorway, .specialArea, .unpavedRoads]
     
     required public init(with options: SYRoutingOptions?) {
         super.init(nibName: nil, bundle: nil)
@@ -43,11 +45,69 @@ public class SYMKRouteOptionsViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         title = LS("routeCompute.options.title")
-        view.backgroundColor = .accentBackground
+//        view.backgroundColor = .accentBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed(_:)))
+        
+        tableView.register(SYMKAvoidTableCell.self, forCellReuseIdentifier: SYMKAvoidTableCell.identifier)
     }
     
     @objc private func doneButtonPressed(_ sender: Any) {
         presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: TableView delegate + dataSource
+    
+    public override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allAvodsList.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let avoid = allAvodsList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: SYMKAvoidTableCell.identifier) as! SYMKAvoidTableCell
+        cell.textLabel?.text = avoid.cellTitle
+        return cell
+    }
+}
+
+private class SYMKAvoidTableCell: UITableViewCell {
+    static let identifier = "avoidCellIdentifier"
+    
+    private let switcher = UISwitch()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        
+        switcher.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(switcher)
+        switcher.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        switcher.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension SYAvoidType {
+    var cellTitle: String {
+        switch self {
+        case .ferries:
+            return LS("avoid.ferries")
+        case .motorway:
+            return LS("avoid.highways")
+        case .toll:
+            return LS("avoid.tollroads")
+        case .specialArea:
+            return LS("avoid.specialArea")
+        case .unpavedRoads:
+            return LS("avoid.unpaved")
+        default:
+            return ""
+        }
     }
 }
