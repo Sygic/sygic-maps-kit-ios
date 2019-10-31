7c2cab9f62f8336e95bc55af8194cd84ee91fdd7
jcalik <jcalik@sygic.com>
2019-10-31 12:52:18 +0100

    search url override is needed no more

--- SygicMapsKit/Classes/SYMKSdkManager.swift
+++ SygicMapsKit/Classes/SYMKSdkManager.swift
@@ -119,7 +119,6 @@ public class SYMKSdkManager {
         var settings: SYMKSdkSettings = [:]
         settings["Authentication"] = ["app_key": SYMKApiKeys.appKey,
                                       "app_secret": SYMKApiKeys.appSecret]
-        settings["Online"] = ["Search": ["url": "https://search.api.sygic.com"]]
         if let skins = customSkinPath {
             settings["StorageFolders"] = ["skin": skins]
         }
